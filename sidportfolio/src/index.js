6b3d65d3564cdf1416419039004d748ad8878dbd
siddharth <siddharthsekhar1111@gmail.com>
2024-12-18 22:23:09 +0530

    added footer and changed background color of the whole screen to actual black

--- sidportfolio/src/index.js
+++ sidportfolio/src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Header from './Header/Header';
-import PhotoSkills from './PhotoSkills/PhotoSkills';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./Header/Header";
+import PhotoSkills from "./PhotoSkills/PhotoSkills";
+import Footer from "./Footer/Footer";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Header/>
-    < PhotoSkills/>
+    <Header />
+    <PhotoSkills />
+    <Footer />
   </React.StrictMode>
 );
 
@@ -16,4 +18,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-  
