1a433be2ff500a1686a0b9388abf9537d04f4c3a
siddharth <siddharthsekhar1111@gmail.com>
2024-12-18 22:01:40 +0530

    added hero/skills section

--- sidportfolio/src/index.js
+++ sidportfolio/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './Header/Header';
+import PhotoSkills from './PhotoSkills/PhotoSkills';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header/>
+    < PhotoSkills/>
   </React.StrictMode>
 );
 
@@ -14,3 +16,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+  
